fd98725acf4ec75550f16aac0ba7a9af72e8435f
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-05-22 19:41:11 +0530

    update website

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,4 @@
 import { React, useState } from "react";
-
-const Navbar = () => {
-  return <div>Navbar</div>;
-};
-
-export default Navbar;
+import { Link } from "react-router-dom";
+import { RiSearch2Line, RiShoppingCartLine, RiMenuLine } from "react-icons/ri";
+import { FaTimes } from "react-icons/fa";
