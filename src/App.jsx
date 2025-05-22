188156037d280d79ab31c1d58da212596acdc84e
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-05-22 19:38:15 +0530

    update website

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { React } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./App.css";
+import { LandingPages } from "./Components";
 
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/" element={<Navbar />} />
+          <Route path="/" element={<LandingPages />} />
         </Routes>
       </Router>
     </div>
