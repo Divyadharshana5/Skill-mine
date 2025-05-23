32c575b6c05c2e781b8702239ecd37ca8249f588
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-05-23 19:39:56 +0530

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { React } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./App.css";
-import { LandingPages } from "./Components";
+import { LandingPages } from "./Components/LandingPages";
 
 function App() {
   return (
