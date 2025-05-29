884f95b8db75b0ec5b717ed07ce45492feb7adf1
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-05-29 18:59:48 +0530

    update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -100,10 +100,9 @@ const Navbar = () => {
             <RiMenuLine color="#000" size={25} />
           </button>
           <span className="hidden lg:flex items-center gap-8 lg:border-1 border-gray-100 pl-5 ml-5 whitespace-nowrap">
-            <Link
-              to=" "
-              className="text-lg font-medium hover:text-purple-500"
-            ></Link>
+            <Link to=" " className="text-lg font-medium hover:text-purple-500">
+              Log In
+            </Link>
           </span>
         </div>
       </nav>
