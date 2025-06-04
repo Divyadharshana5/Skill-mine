d34cb868aa1d4e49d2114abbc96e0a44a1dbd026
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-06-04 18:59:17 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { React } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPages from "./Components/LandingPages";
 import "./App.css";
+import "@fontsource/jost";
+import "@fontsource/jost/700.css";
 
 function App() {
   return (
