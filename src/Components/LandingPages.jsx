cfa4a6d144c6812b7d63ecde3a0f938c09d932f8
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-06-05 09:51:56 +0530

    update LandingPages.jsx

--- src/Components/LandingPages.jsx
+++ src/Components/LandingPages.jsx
@@ -15,7 +15,7 @@ const LandingPages = () => {
             alt=""
           />
           <div className="absolute z-20 w-full h-full bg-[#00000093]"></div>
-          <div className="flex lg:flex-row flex-col items-center justify-center lg:justify-start"></div>
+          <div className="flex lg:flex-row flex-col items-center justify-center lg:justify-start gap-10 relative w-full h-full max-w-[1400p]"></div>
         </div>
       </main>
     </div>
